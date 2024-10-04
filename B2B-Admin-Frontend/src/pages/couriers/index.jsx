e54ca6400e0ace47db0d5065e6dcceb78657aef7
simal <simalchaudhari>
2024-10-04 16:30:44 +0530

    Added changes

---
+++ B2B-Admin-Frontend/src/pages/couriers/index.jsx
@@ -0,0 +1,17 @@
+import { ProductListView } from "src/sections/product/view";
+import { Helmet } from 'react-helmet-async';
+import { CONFIG } from 'src/config-global';
+
+const metadata = { title: `Cpuriers - ${CONFIG.site.name}` };
+
+export default function Page() {
+    return (
+        <>
+            <Helmet>
+                <title> {metadata.title}</title>
+            </Helmet>
+
+            <ProductListView />
+        </>
+    );
+}
