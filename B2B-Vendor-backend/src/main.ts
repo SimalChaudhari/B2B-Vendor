f30597f5418776912cdf34513c5d56a167390519
SimalChaudhari <simalchaudhari20496@gmail.com>
2024-10-11 10:10:44 +0530

    Fix Backend Bug

--- B2B-Vendor-backend/src/main.ts
+++ B2B-Vendor-backend/src/main.ts
@@ -4,14 +4,26 @@ import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from 'utils/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  try {
+    const app = await NestFactory.create(AppModule);
 
-  // Enable CORS
-  app.enableCors({
-    origin: 'http://localhost:3030', // Replace with your frontend URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Optional, if you need to include credentials
-  });
-  await app.listen(3000);
+    // Enable CORS
+    app.enableCors({
+      origin: 'http://localhost:3030', // Replace with your frontend URL
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true, // Optional, if you need to include credentials
+    });
+
+    // Other configurations such as global pipes or filters can go here
+    app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalFilters(new HttpExceptionFilter());
+
+    await app.listen(3000);
+    console.log('Server is running on http://localhost:3000');
+  } catch (error) {
+    console.error('Error starting the server:', error);
+    process.exit(1); // Exit the process with failure
+  }
 }
+
 bootstrap();
