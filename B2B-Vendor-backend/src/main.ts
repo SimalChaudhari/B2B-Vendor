457c527f0cb177fdf8935e191d23af6afb7d1817
zangula-dev <simalc@zangula.com>
2024-11-22 10:29:20 +0530

    Add

--- B2B-Vendor-backend/src/main.ts
+++ B2B-Vendor-backend/src/main.ts
@@ -1,25 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { HttpExceptionFilter } from 'utils/http-exception.filter';
 
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
 
+    // Get environment variables with fallback defaults
+    const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3030';
+    const port = process.env.PORT || 3000;
+
     // Enable CORS
     app.enableCors({
-      origin: 'http://localhost:3030', // Replace with your frontend URL
+      origin: frontendUrl,
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true, // Optional, if you need to include credentials
+      credentials: true,
     });
 
-    // Other configurations such as global pipes or filters can go here
-    // app.useGlobalPipes(new ValidationPipe());
-    // app.useGlobalFilters(new HttpExceptionFilter());
+    await app.listen(port);
 
-    await app.listen(3000);
-    console.log('Server is running on http://localhost:3000');
   } catch (error) {
     console.error('Error starting the server:', error);
     process.exit(1); // Exit the process with failure
