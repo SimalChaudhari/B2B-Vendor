86fbe347efbb775776e3ecebd13ca1cea6b30834
zangula-dev <simalc@zangula.com>
2024-11-25 14:01:37 +0530

    Add

--- B2B-Vendor-backend/src/app.module.ts
+++ B2B-Vendor-backend/src/app.module.ts
@@ -26,18 +26,6 @@ import { VendorModule } from './vendors/vendor.module';
       ssl: false, // Use SSL for production
     }),
   
-
-  // TypeOrmModule.forRoot({
-  //   type: 'postgres',
-  //   host: process.env.DATABASE_HOST,
-  //   port: 5432,
-  //   username: process.env.DATABASE_USER,
-  //   password: process.env.DATABASE_PASSWORD,
-  //   database: process.env.DATABASE_NAME,
-  //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  //   synchronize: true, // Set to false in production
-
-  // }),
   UserModule,
   AuthModule,
   AddressesModule,
