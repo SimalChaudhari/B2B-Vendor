21cdf7d587ac9239c82c510b0e15b0934d665c7e
zangula-dev <simalc@zangula.com>
2024-11-28 18:26:16 +0530

    AdD

--- B2B-Vendor-backend/src/app.module.ts
+++ B2B-Vendor-backend/src/app.module.ts
@@ -11,9 +11,9 @@ import { LedgerModule } from './ledger/ledger.module';
 import { OrderModule } from './order/order.module';
 import { SettingModule } from './settings/setting.module';
 import { StockModule } from './stock/stock.module';
-import { SyncLogModule } from './sync-log/sync-log.module';
 import { UserModule } from './user/users.module';
 import { VendorModule } from './vendors/vendor.module';
+import { SyncLogModule } from 'sync-log/sync-log.module';
 
 @Module({
   imports: [
