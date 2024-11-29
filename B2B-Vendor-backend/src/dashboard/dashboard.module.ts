6b1a1a78861c694e7ff50ef0b09a4d21cd14f2cd
zangula-dev <simalc@zangula.com>
2024-11-29 10:07:45 +0530

    Add

--- B2B-Vendor-backend/src/dashboard/dashboard.module.ts
+++ B2B-Vendor-backend/src/dashboard/dashboard.module.ts
@@ -3,12 +3,11 @@ import { Module } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { DashboardController } from './dashboard.controller';
 import { ItemModule } from './../fetch-products/item.module';
-
 import { UserModule } from 'user/users.module';
 import { OrderModule } from 'order/order.module';
 @Module({
   imports: [ItemModule, UserModule, OrderModule],
-providers: [DashboardService],
+  providers: [DashboardService],
   controllers: [DashboardController],
 })
-export class DashboardModule {}
+export class DashboardModule { }
