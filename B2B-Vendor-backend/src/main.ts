742e554c65ac37161dc790c66fd8ac5f394481fb
zangula-dev <simalc@zangula.com>
2024-11-29 13:16:38 +0530

    add

--- B2B-Vendor-backend/src/main.ts
+++ B2B-Vendor-backend/src/main.ts
@@ -1,13 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import express from 'express';
-import { Logger } from '@nestjs/common';
 
 const server = express();
 
 async function bootstrap() {
   try {
-
+    
     const app = await NestFactory.create(AppModule);
     // Get environment variables with fallback defaults
     const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3030';
@@ -18,12 +17,6 @@ async function bootstrap() {
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       credentials: true,
     });
-
-    // Log all routes
-    const server = app.getHttpServer();
-    const router = server._events.request._router;
-    Logger.log(router.stack.map((layer: { route: { path: any; }; }) => layer.route?.path).filter(Boolean));
-
 
     await app.listen(port);
     console.log(`Server is running on: http://localhost:${port}`); // Log the port
