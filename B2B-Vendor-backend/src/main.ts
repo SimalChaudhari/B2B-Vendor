ec85f4f85bffe8a3012634368bdfe5827509d992
zangula-dev <simalc@zangula.com>
2024-11-29 12:33:33 +0530

    Add

--- B2B-Vendor-backend/src/main.ts
+++ B2B-Vendor-backend/src/main.ts
@@ -1,16 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import express from 'express';
-
-const server = express();
 
 async function bootstrap() {
   try {
-    
     const app = await NestFactory.create(AppModule);
+
     // Get environment variables with fallback defaults
     const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3030';
     const port = process.env.PORT || 3000;
+
     // Enable CORS
     app.enableCors({
       origin: frontendUrl,
@@ -18,15 +16,14 @@ async function bootstrap() {
       credentials: true,
     });
 
+    // Start the application
     await app.listen(port);
     console.log(`Server is running on: http://localhost:${port}`); // Log the port
 
   } catch (error) {
+    console.error('Failed to start the application', error);
     process.exit(1); // Exit the process with failure
   }
-
 }
 
 bootstrap();
-
-export default server;
