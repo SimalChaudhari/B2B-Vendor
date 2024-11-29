9cab87d3412ccea8517ba6e6a8390eeb88c82acb
zangula-dev <simalc@zangula.com>
2024-11-29 11:40:06 +0530

    add

--- B2B-Vendor-backend/src/app.module.ts
+++ B2B-Vendor-backend/src/app.module.ts
@@ -19,7 +19,7 @@ import { SyncLogModule } from './sync-log/sync-log.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: "postgresql://neondb_owner:5Ut6eOIPlkGr@ep-soft-poetry-a1bct69b.ap-southeast-1.aws.neon.tech:5432/b2b_vendor?sslmode=require",
+      url: "postgresql://neondb_owner:5Ut6eOIPlkGr@ep-soft-poetry-a1bct69b.ap-southeast-1.aws.neon.tech/b2b_vendor?sslmode=require",
       autoLoadEntities: true,
       synchronize: true,
       ssl: true, // Enable SSL
