30cd322ddb8f41432fb0135f8a5bf6e86e3aaec5
zangula-dev <simalc@zangula.com>
2024-11-30 11:16:15 +0530

    Add

--- B2B-Vendor-backend/src/main.ts
+++ B2B-Vendor-backend/src/main.ts
@@ -5,7 +5,6 @@ async function bootstrap() {
   try {
    
     const app = await NestFactory.create(AppModule);
-    const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3030';
     const port = process.env.PORT || 3000;
     // Enable CORS
     app.enableCors({
