0a9252094042e3ab58a81343b02d2067916093f7
zangula-dev <simalc@zangula.com>
2024-12-17 15:11:37 +0530

    Add

--- B2B-Vendor-backend/src/main.ts
+++ B2B-Vendor-backend/src/main.ts
@@ -2,16 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  try {
-   
-    const app = await NestFactory.create(AppModule);
-    const port = process.env.PORT || 3000;
-    await app.listen(port);
-    console.log(`Server is running on: http://localhost:${port}`); // Log the port
-
-  } catch (error) {
-    process.exit(1); // Exit the process with failure
-  }
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);  // Listen on port 3000
+  console.log('Application is running on: http://localhost:3000');
 }
-bootstrap();
 
+bootstrap();
