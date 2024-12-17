abca4358518c36310b8de6455e3c4286d5395d9a
zangula-dev <simalc@zangula.com>
2024-12-17 15:03:58 +0530

    add

--- B2B-Vendor-backend/src/vercel.ts
+++ B2B-Vendor-backend/src/vercel.ts
@@ -1,32 +1,32 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { INestApplication } from '@nestjs/common';
+// import { VercelRequest, VercelResponse } from '@vercel/node';
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { INestApplication } from '@nestjs/common';
 
 
-let cachedApp: INestApplication<any>;
+// let cachedApp: INestApplication<any>;
 
-async function buildNestApp() {
-  if (!cachedApp) {
-    cachedApp = await NestFactory.create(AppModule);
-    cachedApp.enableCors({
-      origin:"*", // Frontend URL
-      methods: 'GET, POST, PUT, DELETE',
-      credentials: true,
-    });
-  }
-  return cachedApp;
-}
+// async function buildNestApp() {
+//   if (!cachedApp) {
+//     cachedApp = await NestFactory.create(AppModule);
+//     cachedApp.enableCors({
+//       origin:"*", // Frontend URL
+//       methods: 'GET, POST, PUT, DELETE',
+//       credentials: true,
+//     });
+//   }
+//   return cachedApp;
+// }
 
-export default async (req: VercelRequest, res: VercelResponse) => {
-  const app = await buildNestApp();
-  // Handle CORS in response headers
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+// export default async (req: VercelRequest, res: VercelResponse) => {
+//   const app = await buildNestApp();
+//   // Handle CORS in response headers
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
 
-  // This will call the NestJS app
-  await app.init();
-  return app.getHttpAdapter().getInstance()(req, res);
-};
+//   // This will call the NestJS app
+//   await app.init();
+//   return app.getHttpAdapter().getInstance()(req, res);
+// };
